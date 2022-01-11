0997270e5a99010fc3171d31ae636be11475aa10
Nicolas Gauthier <nicolas.gauthier@lacapitale.com>
2022-01-10 20:32:53 -0500

    frame for shiny using shinydashboard and leaflet

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -9,16 +9,89 @@
 
 library(shiny)
 library(DT)
+library(raster) 
+library(data.table)
 
+mat <- raster::brick("C:/Users/nicol/OneDrive/Documents/GitHub/climR-data/BC/mat_1961-1990.tif")
+rat <- subset(mat, 1)
+pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(rat),
+                    na.color = "transparent")
+
+iris_dt <- as.data.table(iris)[1:12]
+iris_dt[, month := 1:12]
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   
-  output$result_dt <- renderDT({
-    datatable(iris)
+  output$annual_dt <- renderDT({
+    datatable(iris_dt[1],
+              rownames = FALSE,
+              options=list(iDisplayLength=1,
+                           bLengthChange=0,
+                           bFilter=0,
+                           bInfo=0,
+                           bPaginate = 0,
+                           initComplete = JS("function(settings, json) {$(this.api().table().header()).css({'background-color' : '#3c8dbc', 'color' : 'white'});}")
+                           #bAutoWidth=0,                            # automatic column width calculation, disable if passing column width via aoColumnDefs
+                           #aoColumnDefs = list(list(sWidth="300px", aTargets=c(list(0),list(1))))    # custom column size                       
+              )
+              )
   })
   
-  output$map <- renderPlot({
-    ggplot(iris , aes(x = Petal.Width, y = Sepal.Width)) + geom_point()
+  output$seasonal_dt <- renderDT({
+    datatable(iris_dt[1],
+              rownames = FALSE,
+              options=list(iDisplayLength=1,
+                           bLengthChange=0,
+                           bFilter=0,
+                           bInfo=0,
+                           bPaginate = 0,
+                           initComplete = JS("function(settings, json) {$(this.api().table().header()).css({'background-color' : '#3c8dbc', 'color' : 'white'});}")
+                           )
+    )
   })
+
+  output$monthly_dt <- renderDT({
+    datatable(iris_dt,
+              rownames = FALSE,
+              options=list(iDisplayLength=12,
+                           bLengthChange=0,
+                           bFilter=0,
+                           bInfo=0,
+                           bPaginate = 0,
+                           initComplete = JS("function(settings, json) {$(this.api().table().header()).css({'background-color' : '#3c8dbc', 'color' : 'white'});}")
+                           )
+    )
+  })
+  
+  output$monthly_subplot <- renderPlotly({
+  monthly_plots <- list()
+  i <- 1
+    for (variable in names(iris_dt)) {
+      if (class(iris_dt[[variable]]) == "numeric" & variable != "month") {
+        monthly_plots[[i]] <- plot_ly(x = iris_dt[["month"]], y = iris_dt[[variable]], type = 'scatter', mode = 'lines+markers', name = variable)
+          
+      }
+      i <- i + 1
+    }
+  subplot(monthly_plots, nrows = 4)
+  })
+  
+  
+  output$map <- renderLeaflet({
+    leaflet() %>% addTiles() %>%
+      addRasterImage(rat, colors = pal, opacity = 0.8) %>%
+      addLegend(pal = pal, values = values(rat),
+                title = "mat_1961-1990")
+  })
+  
+  
+  observeEvent(input$map_click, {
+    click <- input$map_click
+    leafletProxy("map") |> clearMarkers()
+    leafletProxy("map") |> addMarkers(lng = click$lng, lat = click$lat)
+    updateNumericInput(session, "latitude", value = click$lat)
+    updateNumericInput(session, "longitude", value = click$lng)
+  })
+  
 
 })
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -8,65 +8,128 @@
 #
 
 library(shiny)
+library(shinydashboard)
 library(DT)
+library(leaflet)
+library(plotly)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  titlePanel("ClimateNA Map"),
-  sidebarLayout(
-    sidebarPanel(width = 5,
-                   fluidRow(
-                     column(width = 6,
-                            numericInput(inputId = "latitude", label = "Latitude", min = -90, max = 90, value = 48.98)
-                     ),
-                     column(width = 6,
-                            numericInput(inputId = "longitude", label = "Longitude", min = -180, max = 180, value = -115.02)
-                     )
-                   ),
-                   fluidRow(
-                     column(width = 6,
-                            numericInput(inputId = "elevation", label = "Elevation (m)", min = -430, max = 8848, value = 1000)
-                     ),
-                     column(width = 6,
-                            selectInput(inputId = "historical", label = "Historical", choices = c("Normal_1961_1990", "Normal_1901_1930"))
-                     )
-                   ),
-                   fluidRow(
-                     column(width = 12,
-                            selectInput(inputId = "future", label = "Future", choices = c("Select a GCM period", "13GCMs_ensemble_ssp126_2011-2040.gcm"))
-                     )
-                   ),
-                   br(),
-                   fluidRow(
-                     column(width = 4,
-                            actionButton(inputId = "tutorial", label = "Quick Tutorial",  width = "80%")
-                     ),
-                     column(width = 4,
-                            actionButton(inputId = "help", label = "Help", width = "80%")
-                     ),
-                     column(width = 4,
-                            actionButton(inputId = "calculate", label = "Calculate",  width = "80%")
-                     )
-                   ),
-                   br(),
-                   br(),
-                   br(),
-                   fluidRow(
-                     column(width = 12,
-                            DTOutput(outputId = "result_dt")
-                     )
-                   ),
-                   br(),
-                   fluidRow(
-                     column(width = 1,
-                            actionButton(inputId = "save", label = "Save")
-                     ),
-                     column(width = 1,
-                            actionButton(inputId = "clear", label = "Clear")
-                     )
-                   )
-              ),
-    mainPanel("main panel", width = 7,
-              plotOutput(outputId = "map", height = "940px"))
+
+shinyUI(
+  dashboardPage(
+    dashboardHeader(title = "ClimateNA app"),
+    dashboardSidebar(
+      numericInput(inputId = "latitude", label = "Latitude", min = -90, max = 90, value = 48.98),
+      numericInput(inputId = "longitude", label = "Longitude", min = -180, max = 180, value = -115.02),
+      numericInput(inputId = "elevation", label = "Elevation (m)", min = -430, max = 8848, value = 1000),
+      selectInput(inputId = "historical", label = "Historical", choices = c("Normal_1961_1990", "Normal_1901_1930")),
+      selectInput(inputId = "future", label = "Future", choices = c("Select a GCM period", "13GCMs_ensemble_ssp126_2011-2040.gcm"))
+    ),
+    dashboardBody(
+      fluidRow(
+        column(
+          width = 7,
+          box(
+            title = "Annual Variables",
+            width = 12,
+            DTOutput(outputId = "annual_dt")
+          ),
+          br(),
+          br(),
+          br(),
+          box(
+            title = "Seasonal Variables",
+            width = 12,
+            DTOutput(outputId = "seasonal_dt")
+          ),
+          br(),
+          br(),
+          br(),
+          tabBox(
+            title = "Monthly Variables",
+            side = 'right',
+            width = 12,
+            id = "monthly_tabset",
+            tabPanel(
+              title = "Table",
+              DTOutput(outputId = "monthly_dt")
+            ),
+            tabPanel(
+              title = "Plots",
+              plotlyOutput(outputId = "monthly_subplot")
+            )
+          )
+        ),
+        column(
+          width = 5,
+          box(
+            width = 12,
+            leafletOutput(outputId = "map", height = 1080)
+          )
+        )
+      )
+    )
   )
-))
+)
+
+
+
+# shinyUI(fluidPage(
+#   titlePanel("ClimateNA Map"),
+#   sidebarLayout(
+#     sidebarPanel(width = 5,
+#                    fluidRow(
+#                      column(width = 6,
+#                             numericInput(inputId = "latitude", label = "Latitude", min = -90, max = 90, value = 48.98)
+#                      ),
+#                      column(width = 6,
+#                             numericInput(inputId = "longitude", label = "Longitude", min = -180, max = 180, value = -115.02)
+#                      )
+#                    ),
+#                    fluidRow(
+#                      column(width = 6,
+#                             numericInput(inputId = "elevation", label = "Elevation (m)", min = -430, max = 8848, value = 1000)
+#                      ),
+#                      column(width = 6,
+#                             selectInput(inputId = "historical", label = "Historical", choices = c("Normal_1961_1990", "Normal_1901_1930"))
+#                      )
+#                    ),
+#                    fluidRow(
+#                      column(width = 12,
+#                             selectInput(inputId = "future", label = "Future", choices = c("Select a GCM period", "13GCMs_ensemble_ssp126_2011-2040.gcm"))
+#                      )
+#                    ),
+#                    br(),
+#                    fluidRow(
+#                      column(width = 4,
+#                             actionButton(inputId = "tutorial", label = "Quick Tutorial",  width = "80%")
+#                      ),
+#                      column(width = 4,
+#                             actionButton(inputId = "help", label = "Help", width = "80%")
+#                      ),
+#                      column(width = 4,
+#                             actionButton(inputId = "calculate", label = "Calculate",  width = "80%")
+#                      )
+#                    ),
+#                    br(),
+#                    br(),
+#                    br(),
+#                    fluidRow(
+#                      column(width = 12,
+#                             DTOutput(outputId = "result_dt")
+#                      )
+#                    ),
+#                    br(),
+#                    fluidRow(
+#                      column(width = 1,
+#                             actionButton(inputId = "save", label = "Save")
+#                      ),
+#                      column(width = 1,
+#                             actionButton(inputId = "clear", label = "Clear")
+#                      )
+#                    )
+#               ),
+#     mainPanel("main panel", width = 7,
+#               plotOutput(outputId = "map", height = "940px"))
+#   )
+# ))
