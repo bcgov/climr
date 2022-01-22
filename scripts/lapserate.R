ad17e70cec3207e3f14a04263f05cda373e58c5e
Bruno Tremblay <meztez@neoxone.com>
2022-01-21 23:53:18 -0500

    sample lapse rates from Normal_1961_1990MP

--- scripts/lapserate.R
+++ scripts/lapserate.R
@@ -19,42 +19,42 @@ dem <- raster::raster("./inputs/digitalElevationModel/dem2_WNA.asc")
 
 # do it for all 36 variables
 targets <- list(
-  tmax01 = raster::raster("./inputs/Normal_1961_1990MP/Tmax01.asc"),
-  tmax02 = raster::raster("./inputs/Normal_1961_1990MP/Tmax02.asc"),
-  tmax03 = raster::raster("./inputs/Normal_1961_1990MP/Tmax03.asc"),
-  tmax04 = raster::raster("./inputs/Normal_1961_1990MP/Tmax04.asc"),
-  tmax05 = raster::raster("./inputs/Normal_1961_1990MP/Tmax05.asc"),
-  tmax06 = raster::raster("./inputs/Normal_1961_1990MP/Tmax06.asc"),
-  tmax07 = raster::raster("./inputs/Normal_1961_1990MP/Tmax07.asc"),
-  tmax08 = raster::raster("./inputs/Normal_1961_1990MP/Tmax08.asc"),
-  tmax09 = raster::raster("./inputs/Normal_1961_1990MP/Tmax09.asc"),
-  tmax10 = raster::raster("./inputs/Normal_1961_1990MP/Tmax10.asc"),
-  tmax11 = raster::raster("./inputs/Normal_1961_1990MP/Tmax11.asc"),
-  tmax12 = raster::raster("./inputs/Normal_1961_1990MP/Tmax12.asc"),
-  tmin01 = raster::raster("./inputs/Normal_1961_1990MP/Tmin01.asc"),
-  tmin02 = raster::raster("./inputs/Normal_1961_1990MP/Tmin02.asc"),
-  tmin03 = raster::raster("./inputs/Normal_1961_1990MP/Tmin03.asc"),
-  tmin04 = raster::raster("./inputs/Normal_1961_1990MP/Tmin04.asc"),
-  tmin05 = raster::raster("./inputs/Normal_1961_1990MP/Tmin05.asc"),
-  tmin06 = raster::raster("./inputs/Normal_1961_1990MP/Tmin06.asc"),
-  tmin07 = raster::raster("./inputs/Normal_1961_1990MP/Tmin07.asc"),
-  tmin08 = raster::raster("./inputs/Normal_1961_1990MP/Tmin08.asc"),
-  tmin09 = raster::raster("./inputs/Normal_1961_1990MP/Tmin09.asc"),
-  tmin10 = raster::raster("./inputs/Normal_1961_1990MP/Tmin10.asc"),
-  tmin11 = raster::raster("./inputs/Normal_1961_1990MP/Tmin11.asc"),
-  tmin12 = raster::raster("./inputs/Normal_1961_1990MP/Tmin12.asc"),
-  ppt01  = raster::raster("./inputs/Normal_1961_1990MP/PPT01.asc"),
-  ppt02  = raster::raster("./inputs/Normal_1961_1990MP/PPT02.asc"),
-  ppt03  = raster::raster("./inputs/Normal_1961_1990MP/PPT03.asc"),
-  ppt04  = raster::raster("./inputs/Normal_1961_1990MP/PPT04.asc"),
-  ppt05  = raster::raster("./inputs/Normal_1961_1990MP/PPT05.asc"),
-  ppt06  = raster::raster("./inputs/Normal_1961_1990MP/PPT06.asc"),
-  ppt07  = raster::raster("./inputs/Normal_1961_1990MP/PPT07.asc"),
-  ppt08  = raster::raster("./inputs/Normal_1961_1990MP/PPT08.asc"),
-  ppt09  = raster::raster("./inputs/Normal_1961_1990MP/PPT09.asc"),
-  ppt10  = raster::raster("./inputs/Normal_1961_1990MP/PPT10.asc"),
-  ppt11  = raster::raster("./inputs/Normal_1961_1990MP/PPT11.asc"),
-  ppt12  = raster::raster("./inputs/Normal_1961_1990MP/PPT12.asc")
+  Tmax01 = raster::raster("./inputs/Normal_1961_1990MP/Tmax01.asc"),
+  Tmax02 = raster::raster("./inputs/Normal_1961_1990MP/Tmax02.asc"),
+  Tmax03 = raster::raster("./inputs/Normal_1961_1990MP/Tmax03.asc"),
+  Tmax04 = raster::raster("./inputs/Normal_1961_1990MP/Tmax04.asc"),
+  Tmax05 = raster::raster("./inputs/Normal_1961_1990MP/Tmax05.asc"),
+  Tmax06 = raster::raster("./inputs/Normal_1961_1990MP/Tmax06.asc"),
+  Tmax07 = raster::raster("./inputs/Normal_1961_1990MP/Tmax07.asc"),
+  Tmax08 = raster::raster("./inputs/Normal_1961_1990MP/Tmax08.asc"),
+  Tmax09 = raster::raster("./inputs/Normal_1961_1990MP/Tmax09.asc"),
+  Tmax10 = raster::raster("./inputs/Normal_1961_1990MP/Tmax10.asc"),
+  Tmax11 = raster::raster("./inputs/Normal_1961_1990MP/Tmax11.asc"),
+  Tmax12 = raster::raster("./inputs/Normal_1961_1990MP/Tmax12.asc"),
+  Tmin01 = raster::raster("./inputs/Normal_1961_1990MP/Tmin01.asc"),
+  Tmin02 = raster::raster("./inputs/Normal_1961_1990MP/Tmin02.asc"),
+  Tmin03 = raster::raster("./inputs/Normal_1961_1990MP/Tmin03.asc"),
+  Tmin04 = raster::raster("./inputs/Normal_1961_1990MP/Tmin04.asc"),
+  Tmin05 = raster::raster("./inputs/Normal_1961_1990MP/Tmin05.asc"),
+  Tmin06 = raster::raster("./inputs/Normal_1961_1990MP/Tmin06.asc"),
+  Tmin07 = raster::raster("./inputs/Normal_1961_1990MP/Tmin07.asc"),
+  Tmin08 = raster::raster("./inputs/Normal_1961_1990MP/Tmin08.asc"),
+  Tmin09 = raster::raster("./inputs/Normal_1961_1990MP/Tmin09.asc"),
+  Tmin10 = raster::raster("./inputs/Normal_1961_1990MP/Tmin10.asc"),
+  Tmin11 = raster::raster("./inputs/Normal_1961_1990MP/Tmin11.asc"),
+  Tmin12 = raster::raster("./inputs/Normal_1961_1990MP/Tmin12.asc"),
+  PPT01  = raster::raster("./inputs/Normal_1961_1990MP/PPT01.asc"),
+  PPT02  = raster::raster("./inputs/Normal_1961_1990MP/PPT02.asc"),
+  PPT03  = raster::raster("./inputs/Normal_1961_1990MP/PPT03.asc"),
+  PPT04  = raster::raster("./inputs/Normal_1961_1990MP/PPT04.asc"),
+  PPT05  = raster::raster("./inputs/Normal_1961_1990MP/PPT05.asc"),
+  PPT06  = raster::raster("./inputs/Normal_1961_1990MP/PPT06.asc"),
+  PPT07  = raster::raster("./inputs/Normal_1961_1990MP/PPT07.asc"),
+  PPT08  = raster::raster("./inputs/Normal_1961_1990MP/PPT08.asc"),
+  PPT09  = raster::raster("./inputs/Normal_1961_1990MP/PPT09.asc"),
+  PPT10  = raster::raster("./inputs/Normal_1961_1990MP/PPT10.asc"),
+  PPT11  = raster::raster("./inputs/Normal_1961_1990MP/PPT11.asc"),
+  PPT12  = raster::raster("./inputs/Normal_1961_1990MP/PPT12.asc")
 )
 
 # Recycle the borders of matrix to expand the matrix by one cell in each directions
@@ -232,4 +232,16 @@ lapse_rate <- function(dem, targets, replace_NaN = TRUE, as_raster = FALSE, use_
   
   return(func(targets, lapse_rate_redux, x, nr, nc, n, sum_xx, replace_NaN, as_raster))
   
+}
+
+lapse_rates <- lapse_rate(dem, targets, use_parallel = TRUE, as_raster = TRUE)
+
+for (r in seq_len(length(lapse_rates))) {
+  raster::writeRaster(
+    x = as.integer(lapse_rates[[r]] * 1000L),
+    filename = sprintf("./inputs/lapseRates/LRx1000_%s.asc", names(lapse_rates)[r]),
+    overwrite = TRUE,
+    format="ascii",
+    options=c("INTERLEAVE=BAND","COMPRESS=LZW")
+  )
 }
