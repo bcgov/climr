0ace00f2a210943aedb85619c476ed1a78697476
Bruno Tremblay <meztez@neoxone.com>
2022-01-25 08:50:26 -0500

    first downscaling function draft

--- downscale_basepoint.R
+++
@@ -1,7 +0,0 @@
-setMethod("downscale",
-    signature(latitude = "basepoint"),
-    function (latitude, longitude, elevation, historical, future) 
-    {
-        stop("need a definition for the method here")
-    }
-)
