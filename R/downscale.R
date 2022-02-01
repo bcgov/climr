837be620848aacefa18a1cff2c549480a116fc9e
Bruno Tremblay <meztez@neoxone.com>
2022-01-31 19:50:43 -0500

    Mixing terra and raster methods

--- R/downscale.R
+++ R/downscale.R
@@ -1,8 +1,18 @@
-downscale <- function(object, xy, dem, elev = NULL, lapse_rates = TRUE, extra = TRUE, dem = TRUE) {
-  res <- raster::extract(object, xy, method = "bilinear")
-  elev_delta <- elev - raster::extract(dem, xy, method = "simple")
-  lr <- elev_delta * raster::extract(lapse_rates, xy)
-  res+lr
-  # Compute derivatives
-}
+downscale <- function(object, xy, elev = NULL, lapse_rates = NULL, extra = TRUE) {
   
+  res <- terra::extract(object, xy, method = "bilinear")
+  
+  # Lapse rates adjustment
+  if (!is.null(lapse_rates)) {
+    elev_delta <- elev - terra::extract(attr(lapse_rates, "dem"), xy, method = "simple")
+    lr <- elev_delta * terra::extract(lapse_rates, xy)
+    res+lr    
+  }
+
+  if (isTRUE(extra)) {
+    # Compute derivatives (data.table?)  
+  }
+  
+}
+
+# Add additional methods for non terra objects
