28a33304d6dfceb12dda03a129665ff0505e7822
Kiri Daust <34073038+kdaust@users.noreply.github.com>
2022-06-29 19:27:46 -0700

    Example script

---
+++ Kiri_Testing.R
@@ -0,0 +1,38 @@
+remotes::install_github("bcgov/climR-pnw")
+library(climRpnw)
+# Retrieve package data
+data_update()
+
+# Create a normal baseline
+list_normal()
+normal <- normal_input(normal = "Normal_1961_1990MP_BC")
+dem1 <- terra::rast("C:/Users/kirid/AppData/Local/R/cache/R/climRpnw/inputs_pkg/normal/Normal_1961_1990MP_BC/Normal_1961_1990MP_BC.wlrdem.tif")
+# Select GCM
+gcm <- gcm_input(
+  gcm = c("BCC-CSM2-MR"),
+  ssp = c("ssp245"),
+  period = "2041_2060",
+  max_run = 0
+)
+
+# Provide or create a points dataframe (lon, lat, elev)
+n <- 100000
+xyz <- data.frame(lon = runif(n, -125, -120), lat = runif(n, 51, 53), elev = numeric(n))
+xyz[,3] <- terra::extract(dem1, xyz[,1:2], method = "bilinear")[,-1L]
+
+# Use downscale
+results <- downscale(
+  xyz = xyz,
+  normal = normal,
+  gcm = gcm,
+  var = c("Tmax01", "DD5_01")
+)
+
+# Details about available data
+list_data()
+list_gcm()
+list_normal()
+list_period()
+list_run()
+list_ssp()
+list_variables()
