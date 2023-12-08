b28c7486240ed4d18e539f2165d9675a7f56358e
CeresBarros <ceres.barros@gov.bc.ca>
2023-12-07 17:44:01 -0800

    calc_es -> calc_SVP & calc_SVP -> .calc>SVP

--- R/calc_RH.R
+++ R/calc_RH.R
@@ -11,8 +11,8 @@
 #' }
 calc_RH <- function(tmin, tmax) {
   
-  es_tmin <- calc_es(tmin)
-  es_tmax <- calc_es(tmax)
+  es_tmin <- calc_SVP(tmin)
+  es_tmax <- calc_SVP(tmax)
   es_avg = (es_tmin + es_tmax)/2
   return(100 * es_tmin/es_avg)
   
@@ -24,7 +24,7 @@ calc_RH <- function(tmin, tmax) {
 #' @references Hogg, E.H. (1997). Temporal scaling of moisture and the forest-grassland boundary in western Canada. Agricultural and Forest Meteorology, Research on Forest Environmental Influences in a Changing World, 84, 115–122.
 #' @importFrom data.table fifelse
 calc_PET <- function(tave, tmin, tmax, alt){
-  D <- 0.5*(calc_SVP(tmax) - calc_SVP(tmin)) - calc_SVP(tmin - 2.5)
+  D <- 0.5*(.calc_SVP(tmax) - .calc_SVP(tmin)) - .calc_SVP(tmin - 2.5)
   pet <- fifelse(tave > 10, 93 * D * exp(alt/9300),
                  fifelse(tave > -5, (6.2 * tave + 31) * D * exp(alt/9300), 0))
   return(pet)
@@ -36,18 +36,15 @@ calc_PET <- function(tave, tmin, tmax, alt){
 
 #' Calculate Saturated Vapour Pressure at a temperature t
 #' @template t
-calc_es <- function(t) {
-  
-  svp <- calc_SVP(t)
+
+calc_SVP <- function(t) {
+  svp <- .calc_SVP(t)
   i <- which(t < 0)
   svp[i] <- svp[i] * (1 + ( t[i] * 0.01) )
   return(svp)
-  
 }
 
 #' @template t
-calc_SVP <- function(t){
-  
+.calc_SVP <- function(t){
   return(0.6105 * exp((17.273*t)/(t+237.3)))
-  
 }
