716ba4ee117ce0a0c59e0c3f166d9bfc4a909991
CeresBarros <ceres.barros@gov.bc.ca>
2023-12-21 11:33:33 -0800

    more cache tests - no server connection

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -106,5 +106,22 @@ test_that("test cache in custom location", {
 })
 
 test_that("test cache works within same bbox", {
-  browser() ## TODO
+  dbCon <- data_connect()
+  on.exit(poolClose(dbCon), add = TRUE)
+  on.exit(unlink(cache_path(), recursive = TRUE, force = TRUE))
+  
+  xyz <- data.frame(Long = c(-127.70521, -127.62279, -127.56235, -127.7162, 
+                             -127.18585, -127.1254, -126.94957, -126.95507), 
+                    Lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025), 
+                    Elev = c(291L, 296L, 626L, 377L, 424L, 591L, 723L, 633L),
+                    ID = LETTERS[1:8],
+                    Zone = c(rep("CWH",3), rep("CDF",5)),
+                    Subzone = c("vm1","vm2","vs1",rep("mm",3),"dk","dc"))
+  thebb <- get_bb(xyz)
+  
+  cache_clear()
+  normal <- normal_input(dbCon = dbCon, bbox = thebb)
+  normal2 <- normal_input(dbCon = NULL, bbox = thebb)
+  
+  expect_true(compareGeom(normal, normal2, res = TRUE, lyrs = TRUE, stopOnError = FALSE))
 }
