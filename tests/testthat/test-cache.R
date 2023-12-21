a9318554bcdd24fc36c107ecd304feecc74750a5
CeresBarros <ceres.barros@gov.bc.ca>
2023-12-21 10:59:32 -0800

    add xyz and dbCon to test-cache

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -1,4 +1,15 @@
 test_that("test cache in default location", {
+  dbCon <- data_connect()
+  xyz <- data.frame(Long = c(-127.70521, -127.62279, -127.56235, -127.7162, 
+                             -127.18585, -127.1254, -126.94957, -126.95507), 
+                    Lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025), 
+                    Elev = c(291L, 296L, 626L, 377L, 424L, 591L, 723L, 633L),
+                    ID = LETTERS[1:8],
+                    Zone = c(rep("CWH",3), rep("CDF",5)),
+                    Subzone = c("vm1","vm2","vs1",rep("mm",3),"dk","dc"))
+  
+  thebb <- get_bb(xyz)
+  
   expect_identical(cache_path(), R_user_dir("climr", "cache"))
   
   on.exit(unlink(cache_path(), recursive = TRUE, force = TRUE))
