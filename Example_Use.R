bcgov/climr

commit 7b3ae099b5c093127b854e3998e2f47a162700de

Download run info on load, so downscale works offline with cache

Example_Use.R
@@ -3,10 +3,6 @@ library(pool)
 library(data.table)
 library(terra)
 
-set.seed(123)
-dbCon <- data_connect()
-xyz <- data.frame(lon = runif(10, -140, -106), lat = runif(10, 37, 61), elev = runif(10))
-
 ## get bounding box based on input points
 thebb <- get_bb(xyz)
 historic <- historic_input(dbCon, thebb, period = "2001_2020")
@@ -36,8 +32,7 @@ list_historic()
 ds_hist <- climr_downscale(xyz = in_xyz, which_normal = "auto", 
                            historic_period = "2001_2020",
                            return_normal = TRUE, ##put this to TRUE if you want the 1961-1990 period
-                           vars = c("PPT","CMD","CMI","Tave01","Tave07"),
-                           out_spatial = TRUE, plot = "PPT") ##specify desired variables
+                           vars = c("PPT","CMD","CMI","Tave01","Tave07")) ##specify desired variables
 
 
 ##Future periods:

R/climr-package.R
@@ -1,11 +1,32 @@
 # On load, instantiate either as new or from cache
+#' @importFrom data.table fwrite
+#' @importFrom RPostgres dbGetQuery
 #' @noRd
 .onLoad <- function(libname, pkgname) {
-  .climr[["files_uid_db"]] <- character()
-  if (file.exists(uid_db())) {
-    .climr[["files_uid_db"]] <- readRDS(uid_db())
+  if(!dir.exists(paste0(cache_path(), "/run_info/"))){
+    message("Downloading and Caching ESM run info :)")
+    dbCon <- data_connect()
+    if(is.null(dbCon)){
+      warning("Could not connect to server. Only cached normal periods will be available.")
+    }else{
+      dir.create(paste0(cache_path(), "/run_info/"), recursive = TRUE)
+      gcm_period_runs <- dbGetQuery(dbCon, "select distinct mod, scenario, run from esm_layers order by mod, scenario, run;")
+      gcm_ts_runs <- dbGetQuery(dbCon, "select distinct mod, scenario, run from esm_layers_ts order by mod, scenario, run;")
+      gcm_hist_runs <- dbGetQuery(dbCon, "select distinct mod, run from esm_layers_hist order by mod, run;")
+      fwrite(gcm_period_runs, paste0(cache_path(), "/run_info/gcm_period.csv"))
+      fwrite(gcm_ts_runs, paste0(cache_path(), "/run_info/gcm_ts.csv"))
+      fwrite(gcm_hist_runs, paste0(cache_path(), "/run_info/gcm_hist.csv"))
+    }
+
   }
 }
+
+# .onLoad <- function(libname, pkgname) {
+#   .climr[["files_uid_db"]] <- character()
+#   if (file.exists(uid_db())) {
+#     .climr[["files_uid_db"]] <- readRDS(uid_db())
+#   }
+# }
 
 #' Climate variables definition
 "variables"

R/downscale.R
@@ -154,12 +154,12 @@ climr_downscale <- function(xyz, which_normal = c("auto", "BC", "NorAm"), histor
   )
   
   if (which_normal %in% c("BC", "NorAm")) {
-    poolClose(dbCon)
+    if(!is.null(dbCon)) poolClose(dbCon)
     results <- addIDCols(IDCols, results)
     return(results)
   } 
   if ((length(bc_ids) == nrow(xyz_save) | length(bc_ids) < 1)) {
-    poolClose(dbCon)
+    if(!is.null(dbCon)) poolClose(dbCon)
     results <- addIDCols(IDCols, results)
     return(results)
   } else {
@@ -182,7 +182,7 @@ climr_downscale <- function(xyz, which_normal = c("auto", "BC", "NorAm"), histor
       plot = plot
     )
     
-    poolClose(dbCon)
+    if(!is.null(dbCon)) poolClose(dbCon)
     res_all <- rbind(results, results_na)
     res_all <- addIDCols(IDCols, res_all)
     

R/gcm.R
@@ -209,6 +209,10 @@ process_one_gcm2 <- function(gcm_nm, ssp, bbox, period, max_run, dbnames = dbnam
   gcmcode <- dbnames$dbname[dbnames$GCM == gcm_nm]
   gcm_nm <- gsub("-", ".", gcm_nm)
   
+  runs <- fread(paste0(cache_path(), "/run_info/gcm_period.csv"))
+  runs <- sort(unique(runs[mod == gcm_nm & scenario %in% ssp, run]))
+  sel_runs <- runs[1:(max_run + 1L)]
+  
   if (dir.exists(paste0(cache_path(), "/gcm/", gcmcode))) {
     bnds <- fread(paste0(cache_path(), "/gcm/", gcmcode, "/meta_area.csv"))
     setorder(bnds, -numlay)
@@ -223,9 +227,6 @@ process_one_gcm2 <- function(gcm_nm, ssp, bbox, period, max_run, dbnames = dbnam
       if (all(period %in% periods[uid == oldid, period]) & all(ssp %in% ssps[uid == oldid, ssp]) & max_run <= bnds[uid == oldid, max_run]) {
         message("Retrieving from cache...")
         gcm_rast <- rast(paste0(cache_path(), "/gcm/", gcmcode, "/", oldid, ".tif"))
-        runs <- sort(dbGetQuery(dbCon, paste0("select distinct run from esm_layers where mod = '", gcm_nm, "'"))$run)
-        sel_runs <- runs[1:(max_run + 1L)]
-        
         layinfo <- data.table(fullnm = names(gcm_rast))
         layinfo[, c("Mod", "Var", "Month", "Scenario", "Run", "Period1", "Period2") := tstrsplit(fullnm, "_")]
         layinfo[, Period := paste(Period1, Period2, sep = "_")]
@@ -238,8 +239,6 @@ process_one_gcm2 <- function(gcm_nm, ssp, bbox, period, max_run, dbnames = dbnam
     }
   }
   
-  runs <- sort(dbGetQuery(dbCon, paste0("select distinct run from esm_layers where mod = '", gcm_nm, "'"))$run)
-  sel_runs <- runs[1:(max_run + 1L)]
   
   q <- paste0(
     "select fullnm, laynum from esm_layers where mod = '", gcm_nm, "' and scenario in ('", paste(ssp, collapse = "','"),
@@ -287,6 +286,10 @@ process_one_gcm2 <- function(gcm_nm, ssp, bbox, period, max_run, dbnames = dbnam
 process_one_gcm3 <- function(gcm_nm, years, dbCon, bbox, max_run, dbnames = dbnames_hist, cache) { ## need to update to all GCMs
   gcmcode <- dbnames$dbname[dbnames$GCM == gcm_nm]
   
+  runs <- fread(paste0(cache_path(), "/run_info/gcm_hist.csv"))
+  runs <- sort(unique(runs[mod == gcm_nm, run]))
+  sel_runs <- runs[1:(max_run + 1L)]
+  
   if (dir.exists(paste0(cache_path(), "/gcmhist/", gcmcode))) {
     bnds <- fread(paste0(cache_path(), "/gcmhist/", gcmcode, "/meta_area.csv"))
     setorder(bnds, -numlay)
@@ -300,9 +303,6 @@ process_one_gcm3 <- function(gcm_nm, years, dbCon, bbox, max_run, dbnames = dbna
       if (all(years %in% yeardat[uid == oldid, years]) & max_run <= bnds[uid == oldid, max_run]) {
         message("Retrieving from cache...")
         gcm_rast <- rast(paste0(cache_path(), "/gcmhist/", gcmcode, "/", oldid, ".tif"))
-        runs <- sort(dbGetQuery(dbCon, paste0("select distinct run from esm_layers_hist where mod = '", gcm_nm, "'"))$run)
-        sel_runs <- runs[1:(max_run + 1L)]
-        
         layinfo <- data.table(fullnm = names(gcm_rast))
         layinfo[, c("Mod", "Var", "Month", "Run", "Year") := tstrsplit(fullnm, "_")]
         layinfo[, laynum := seq_along(Year)]
@@ -313,9 +313,6 @@ process_one_gcm3 <- function(gcm_nm, years, dbCon, bbox, max_run, dbnames = dbna
       }
     }
   }
-  
-  runs <- sort(dbGetQuery(dbCon, paste0("select distinct run from esm_layers_hist where mod = '", gcm_nm, "'"))$run)
-  sel_runs <- runs[1:(max_run + 1L)]
   
   q <- paste0("select mod, var, month, run, year, laynum from esm_layers_hist where mod = '", gcm_nm, "' and year in ('", paste(years, collapse = "','"), "') and run in ('", paste(sel_runs, collapse = "','"), "')")
   # print(q)
@@ -358,6 +355,10 @@ process_one_gcm3 <- function(gcm_nm, years, dbCon, bbox, max_run, dbnames = dbna
 #' @return a SpatRaster
 process_one_gcm4 <- function(gcm_nm, ssp, period, max_run, dbnames = dbnames_ts, bbox, dbCon, cache) { ## need to update to all GCMs
   gcmcode <- dbnames$dbname[dbnames$GCM == gcm_nm]
+  runs <- fread(paste0(cache_path(), "/run_info/gcm_ts.csv"))
+  runs <- sort(unique(runs[mod == gcm_nm & scenario %in% ssp, run]))
+  if (length(runs) < 1) stop("That GCM isn't in our database yet.")
+  sel_runs <- runs[1:(max_run + 1L)]
   
   if (dir.exists(paste0(cache_path(), "/gcmts/", gcmcode))) {
     bnds <- fread(paste0(cache_path(), "/gcmts/", gcmcode, "/meta_area.csv"))
@@ -373,9 +374,6 @@ process_one_gcm4 <- function(gcm_nm, ssp, period, max_run, dbnames = dbnames_ts,
       if (all(period %in% periods[uid == oldid, period]) & all(ssp %in% ssps[uid == oldid, ssp]) & max_run <= bnds[uid == oldid, max_run]) {
         message("Retrieving from cache...")
         gcm_rast <- rast(paste0(cache_path(), "/gcmts/", gcmcode, "/", oldid, ".tif"))
-        runs <- sort(dbGetQuery(dbCon, paste0("select distinct run from esm_layers_ts where mod = '", gcm_nm, "'"))$run)
-        sel_runs <- runs[1:(max_run + 1L)]
-        
         layinfo <- data.table(fullnm = names(gcm_rast))
         layinfo[, c("Mod", "Var", "Month", "Scenario", "Run", "Year") := tstrsplit(fullnm, "_")]
         layinfo[, laynum := seq_along(fullnm)]
@@ -386,10 +384,6 @@ process_one_gcm4 <- function(gcm_nm, ssp, period, max_run, dbnames = dbnames_ts,
       }
     }
   }
-  
-  runs <- sort(dbGetQuery(dbCon, paste0("select distinct run from esm_layers_ts where mod = '", gcm_nm, "'"))$run)
-  if (length(runs) < 1) stop("That GCM isn't in our database yet.")
-  sel_runs <- runs[1:(max_run + 1L)]
   
   q <- paste0(
     "select fullnm, laynum from esm_layers_ts where mod = '", gcm_nm, "' and scenario in ('", paste(ssp, collapse = "','"),

commit 2a884dadb003552706f412dfa9aac2fd376ff217

fix merge bugs introduced in 7b3ae099b5c093127b854e3998e2f47a162700de

Example_Use.R
@@ -3,6 +3,10 @@ library(pool)
 library(data.table)
 library(terra)
 
+set.seed(123)
+dbCon <- data_connect()
+xyz <- data.frame(lon = runif(10, -140, -106), lat = runif(10, 37, 61), elev = runif(10))
+
 ## get bounding box based on input points
 thebb <- get_bb(xyz)
 historic <- historic_input(dbCon, thebb, period = "2001_2020")
@@ -14,7 +18,7 @@ in_xyz <- data.frame(Long = c(-127.70521, -127.62279, -127.56235, -127.7162,
                      Lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025), 
                      Elev = c(291L, 296L, 626L, 377L, 424L, 591L, 723L, 633L),
                      Zone = c(rep("CWH",3), rep("CDF",5)),
-                     Subzone = c("vm1","vm2","vs1",rep("mm",3),"dk","dc")),
+                     Subzone = c("vm1","vm2","vs1",rep("mm",3),"dk","dc"))
 
 ##show available variables:
 list_variables()
@@ -29,7 +33,9 @@ list_historic()
 ds_hist <- climr_downscale(xyz = in_xyz, which_normal = "auto", 
                            historic_period = "2001_2020",
                            return_normal = TRUE, ##put this to TRUE if you want the 1961-1990 period
-                           vars = c("PPT","CMD","CMI","Tave01","Tave07")) ##specify desired variables
+                           vars = c("PPT","CMD","CMI","Tave01","Tave07"),
+                           out_spatial = TRUE, plot = "PPT") ##specify desired variables
+
 
 
 ##Future periods:
@@ -107,8 +113,8 @@ plot(gcm[[1]]$BCC.CSM2.MR_Tmax_08_ssp370_ensembleMean_2041_2060)
 set.seed(678)
 n <- 40000
 sample_xyz <- data.frame(lon = runif(n, xmin(normalbc$dem2_WNA), xmax(normalbc$dem2_WNA)), 
-                  lat = runif(n, ymin(normalbc$dem2_WNA), ymax(normalbc$dem2_WNA)), 
-                  elev = NA)
+                         lat = runif(n, ymin(normalbc$dem2_WNA), ymax(normalbc$dem2_WNA)), 
+                         elev = NA)
 sample_xyz[, 3] <- terra::extract(normalbc$dem2_WNA, sample_xyz[, 1:2], method = "bilinear")[, -1L]
 
 ##downscale function