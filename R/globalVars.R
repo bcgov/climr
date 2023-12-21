32d30c020776c201eeade9b2515c15cfe2d58ae7
CeresBarros <ceres.barros@gov.bc.ca>
2023-12-21 14:19:02 -0800

    reorder and add missing global vars

--- R/globalVars.R
+++ R/globalVars.R
@@ -1,6 +1,5 @@
 utils::globalVariables(c(
-  "Period", "Period1", "Period2", "Run",
-  "Scenario", "Year", "fullnm", "laynum",
-  "mod", "numlay", "period", "run", "var",
-  "skip_if_not_installed", "..cols" 
+  "..cols", "fullnm", "laynum", "mod", "numlay", "period", 
+  "Period", "Period1", "Period2", "run", "Run",
+  "scenario", "Scenario", "skip_if_not_installed", "var", "Year"
 ))
