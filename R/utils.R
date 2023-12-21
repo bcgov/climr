d046209695f3d2a864ec207fca06c0b869d44b08
CeresBarros <ceres.barros@gov.bc.ca>
2023-12-21 15:02:04 -0800

    mv is_in_box to utils.R

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,18 @@ shush <- function(expr) {
     classes = c("message", "condition")
   )
 }
+
+
+#' Check that new BB is in old BB
+#'
+#' @param newbb TODO
+#' @param oldbb TODO
+#'
+#' @return logical
+is_in_bbox <- function(newbb, oldbb) {
+  if (newbb[1] < oldbb[1] & newbb[2] > oldbb[2] & newbb[3] < oldbb[3] & newbb[4] > oldbb[4]) {
+    TRUE
+  } else {
+    FALSE
+  }
+}
