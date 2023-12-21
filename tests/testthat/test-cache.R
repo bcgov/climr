f66a69d2fafcdde9bfce565da750e55172f30d25
CeresBarros <ceres.barros@gov.bc.ca>
2023-12-21 11:32:07 -0800

    more cache testing - historic

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -50,66 +50,9 @@ test_that("test cache in default location", {
   )
   expect_true("gcm" %in% list.files(cache_path()))
   
-  cache_clear("gcm")
-  expect_false("gcm" %in% list.files(cache_path()))
-  expect_true("normal" %in% list.files(cache_path()))
+  ds_res <- climr_downscale(xyz, which_normal = "BC", historic_period = "2001_2020")
+  expect_true("historic" %in% list.files(cache_path()))
   
-  gcm1 <- gcm_input(
-    dbCon, 
-    thebb,
-    gcm = "BCC-CSM2-MR",
-    ssp = c("ssp126"),
-    period = "2041_2060",
-    max_run = 1
-  )
-  cache_clear("normal")
-  expect_false("normal" %in% list.files(cache_path()))
-  expect_true("gcm" %in% list.files(cache_path()))
-  
-  normal <- normal_input(dbCon = dbCon, bbox = thebb, cache = TRUE)
-  cache_clear()
-  expect_false("normal" %in% list.files(cache_path()))
-  expect_false("gcm" %in% list.files(cache_path()))
-  
-  
-  ## TODO add tests for historic
-  browser()
-  
-}
-
-test_that("test cache in custom location", {
-  options("climr.cache.path" = "~/test_climr/")
-  expect_identical(cache_path(), "~/test_climr/")
-  
-  on.exit(unlink(cache_path(), recursive = TRUE, force = TRUE))
-  
-  cache_clear()
-  normal <- normal_input(dbCon = dbCon, bbox = thebb, cache = FALSE)
-  expect_identical(length(list.files(cache_path())), 0L)
-  
-  cache_clear()
-  gcm1 <- gcm_input(
-    dbCon, 
-    thebb,
-    gcm = "BCC-CSM2-MR",
-    ssp = c("ssp126"),
-    period = "2041_2060",
-    max_run = 1, cache = FALSE
-  )
-  expect_identical(length(list.files(cache_path())), 0L)
-  
-  normal <- normal_input(dbCon = dbCon, bbox = thebb)
-  expect_true("normal" %in% list.files(cache_path()))
-  
-  gcm1 <- gcm_input(
-    dbCon, 
-    thebb,
-    gcm = "BCC-CSM2-MR",
-    ssp = c("ssp126"),
-    period = "2041_2060",
-    max_run = 1
-  )
-  expect_true("gcm" %in% list.files(cache_path()))
   
   cache_clear("gcm")
   expect_false("gcm" %in% list.files(cache_path()))
@@ -127,11 +70,21 @@ test_that("test cache in custom location", {
   expect_false("normal" %in% list.files(cache_path()))
   expect_true("gcm" %in% list.files(cache_path()))
   
-  normal <- normal_input(dbCon = dbCon, bbox = thebb, cache = TRUE)
-  cache_clear()
-  expect_false("normal" %in% list.files(cache_path()))
-  expect_false("gcm" %in% list.files(cache_path()))
+  cache_clear("historic")
+  expect_true("gcm" %in% list.files(cache_path()))
+  expect_false("historic" %in% list.files(cache_path()))
   
+  cache_clear()
+  ds_res <- climr_downscale(xyz, which_normal = "BC", historic_period = "2001_2020", 
+                            gcm_models = list_gcm()[1], gcm_period = "2041_2060")
+  cachedirs <- normalizePath(list.dirs(cache_path(), recursive = FALSE), winslash = "/")
+  test <- all(expecteddirs %in% cachedirs)
+  expect_true(test)
+  
+  cache_clear()
+  cachedirs <- normalizePath(list.dirs(cache_path(), recursive = FALSE), winslash = "/")
+  test <- all(expecteddirs %in% cachedirs)
+  expect_false(test)
 }
 
 
