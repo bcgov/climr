a3cfacf030678aa632328d1add81f97741139365
CeresBarros <ceres.barros@gov.bc.ca>
2023-12-21 12:38:56 -0800

    fix tests

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -1,6 +1,6 @@
 test_that("test cache in default location", {
   dbCon <- data_connect()
-  on.exit(poolClose(dbCon), add = TRUE)
+  on.exit(try(pool::poolClose(dbCon)), add = TRUE)
   xyz <- data.frame(Long = c(-127.70521, -127.62279, -127.56235, -127.7162, 
                              -127.18585, -127.1254, -126.94957, -126.95507), 
                     Lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025), 
@@ -12,10 +12,9 @@ test_that("test cache in default location", {
   thebb <- get_bb(xyz)
   
   expect_identical(cache_path(), R_user_dir("climr", "cache"))
-  on.exit(unlink(cache_path(), recursive = TRUE, force = TRUE), add = TRUE)
   
   expecteddirs <- normalizePath(file.path(cache_path(), c("gcm", "normal", "historic")), winslash = "/")
-
+  
   cache_clear()
   normal <- normal_input(dbCon = dbCon, bbox = thebb, cache = FALSE)
   cachedirs <- normalizePath(list.dirs(cache_path(), recursive = FALSE), winslash = "/")
@@ -88,10 +87,19 @@ test_that("test cache in default location", {
 })
 
 test_that("test cache in custom location", {
+  opts <- options()
   options("climr.cache.path" = "~/test_climr")
+  on.exit(options(opts), add = TRUE)
   expect_identical(cache_path(), "~/test_climr")
   
-  on.exit(unlink(cache_path(), recursive = TRUE, force = TRUE))
+  xyz <- data.frame(Long = c(-127.70521, -127.62279, -127.56235, -127.7162, 
+                             -127.18585, -127.1254, -126.94957, -126.95507), 
+                    Lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025), 
+                    Elev = c(291L, 296L, 626L, 377L, 424L, 591L, 723L, 633L),
+                    ID = LETTERS[1:8],
+                    Zone = c(rep("CWH",3), rep("CDF",5)),
+                    Subzone = c("vm1","vm2","vs1",rep("mm",3),"dk","dc"))
+  
   
   ds_res <- climr_downscale(xyz, which_normal = "BC", historic_period = "2001_2020", 
                             gcm_models = list_gcm()[1], gcm_period = "2041_2060")
@@ -107,9 +115,8 @@ test_that("test cache in custom location", {
 
 test_that("test cache works within same bbox", {
   dbCon <- data_connect()
-  on.exit(poolClose(dbCon), add = TRUE)
-  on.exit(unlink(cache_path(), recursive = TRUE, force = TRUE))
-  
+  on.exit(try(pool::poolClose(dbCon)), add = TRUE)
+
   xyz <- data.frame(Long = c(-127.70521, -127.62279, -127.56235, -127.7162, 
                              -127.18585, -127.1254, -126.94957, -126.95507), 
                     Lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025), 
@@ -123,5 +130,5 @@ test_that("test cache works within same bbox", {
   normal <- normal_input(dbCon = dbCon, bbox = thebb)
   normal2 <- normal_input(dbCon = NULL, bbox = thebb)
   
-  expect_true(compareGeom(normal, normal2, res = TRUE, lyrs = TRUE, stopOnError = FALSE))
+  expect_true(terra::compareGeom(normal, normal2, res = TRUE, lyrs = TRUE, stopOnError = FALSE))
 })
