56e3a883ca1eb1d42b21077a98dcdc5a93386421
CeresBarros <ceres.barros@gov.bc.ca>
2024-01-03 16:26:09 -0800

    more style consistency fixes

--- man-roxygen/period.R
+++ man-roxygen/period.R
@@ -1,2 +1,2 @@
-#' @param period A character vector. Vector of labels of the periods to use.
+#' @param period character. Vector of labels of the periods to use.
 #'   Can be obtained from `list_gcm_period()`. Default to `list_gcm_period()`.
