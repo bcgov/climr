f95af99e57238ad5a1a41adf9b21fa8cbe0d40c9
CeresBarros <ceres.barros@gov.bc.ca>
2024-01-16 15:59:28 -0800

    fix test

--- tests/testthat/test-climr_downscale.R
+++ tests/testthat/test-climr_downscale.R
@@ -100,13 +100,11 @@ test_that("test climr_dowscale with more args", {
   testInit("data.table")
   
   ## a small area
-  xyz <- structure(list(Long = c(-127.70521, -127.62279, -127.56235, -127.7162,
+  xyz <- structure(list(lon = c(-127.70521, -127.62279, -127.56235, -127.7162,
                                  -127.18585, -127.1254, -126.94957, -126.95507),
-                        Lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025),
-                        Elev = c(291L, 296L, 626L, 377L, 424L, 591L, 723L, 633L),
-                        ID = LETTERS[1:8],
-                        Zone = c(rep("CWH",3), rep("CDF",5)),
-                        Subzone = c("vm1","vm2","vs1",rep("mm",3),"dk","dc")),
+                        lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025),
+                        elev = c(291L, 296L, 626L, 377L, 424L, 591L, 723L, 633L),
+                        id = LETTERS[1:8]),
                    row.names = c(NA, -8L), class = "data.frame")
   
   argsCombos <- expand.grid(which_normal = c("auto", "normal_na", "normal_bc"), historic_period = c(NA, "2001_2020"), 
@@ -115,11 +113,29 @@ test_that("test climr_dowscale with more args", {
                             gcm_ts_years = c(NA, "2015:2050"), gcm_hist_years = c(NA, "1950:2010")) |>
     as.data.table()
   
-  out <- apply(argsCombos[7], 1, function(args, xyz) {
-    browser()
+  out <- apply(argsCombos, 1, function(args, xyz) {
     args <- args[!is.na(args)]
     args$xyz <- xyz # coerces to list.
     args$vars <- c("PPT","CMD")  ## for faster results.
+    
+    if (!is.null(args$historic_ts)) {
+      args$historic_ts <- eval(parse(text = args$historic_ts))
+    }
+    if (!is.null(args$gcm_models)) {
+      args$gcm_models <- eval(parse(text = args$gcm_models))
+    }
+    if (!is.null(args$ssp)) {
+      args$ssp <- eval(parse(text = args$ssp))
+    }
+    if (!is.null(args$gcm_period)) {
+      args$gcm_period <- eval(parse(text = args$gcm_period))
+    }
+    if (!is.null(args$gcm_ts_years)) {
+      args$gcm_ts_years <- eval(parse(text = args$gcm_ts_years))
+    }
+    if (!is.null(args$gcm_models)) {
+      args$gcm_hist_years <- eval(parse(text = args$gcm_hist_years))
+    }
     
     out <- try(do.call(climr_downscale, args))
     is(out, "data.frame")
