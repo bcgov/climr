00d66c7655bc9bd587b5093b2646c7bf2ba865aa
CeresBarros <ceres.barros@gov.bc.ca>
2024-01-16 09:46:08 -0800

    minor

--- tests/testthat/test-downscale.R
+++ tests/testthat/test-downscale.R
@@ -5,7 +5,7 @@ test_that("test downscale", {
   on.exit(try(pool::poolClose(dbCon)), add = TRUE)
   
   xyz <- data.frame(lon = c(-127.70521, -127.62279, -127.56235, -127.7162, 
-                          -127.18585, -127.1254, -126.94957, -126.95507), 
+                            -127.18585, -127.1254, -126.94957, -126.95507), 
                     lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025), 
                     elev = c(291L, 296L, 626L, 377L, 424L, 591L, 723L, 633L),
                     id = LETTERS[1:8])
@@ -89,8 +89,7 @@ test_that("test downscale", {
   testthat::expect_true(all.equal(results2, results3))
 })
 
-## here, tet with gcm_hist, gcm_ts and historic
-test_that("test downscale with gcm_hist, gcm_ts, historic and historic_ts", {
+test_that("test downscale outputs with gcm, gcm_hist, gcm_ts, historic and historic_ts", {
   testInit("terra")
   
   dbCon <- data_connect()
