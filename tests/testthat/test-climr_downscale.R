0fe9f99a4964304cb0e69a343ad200186d5b1dba
CeresBarros <ceres.barros@gov.bc.ca>
2024-01-16 18:23:17 -0800

    complete test for climr_downscale arg combos

--- tests/testthat/test-climr_downscale.R
+++ tests/testthat/test-climr_downscale.R
@@ -99,19 +99,27 @@ if (interactive()) {
 test_that("test climr_dowscale with more args", {
   testInit("data.table")
   
-  ## a small area
-  xyz <- structure(list(lon = c(-127.70521, -127.62279, -127.56235, -127.7162,
-                                 -127.18585, -127.1254, -126.94957, -126.95507),
-                        lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025),
-                        elev = c(291L, 296L, 626L, 377L, 424L, 591L, 723L, 633L),
-                        id = LETTERS[1:8]),
-                   row.names = c(NA, -8L), class = "data.frame")
+  ## a small no. of points
+  xyz <- data.frame(lon = c(-127.70521, -127.62279, -127.56235, -127.7162,
+                            -127.18585, -127.1254, -126.94957, -126.95507),
+                    lat = c(55.3557, 55.38847, 55.28537, 55.25721, 54.88135, 54.65636, 54.6913, 54.61025),
+                    elev = c(291L, 296L, 626L, 377L, 424L, 591L, 723L, 633L),
+                    id = LETTERS[1:8],
+                    Zone = c(rep("CWH",3), rep("CDF",5)),
+                    Subzone = c("vm1","vm2","vs1",rep("mm",3),"dk","dc"))
   
-  argsCombos <- expand.grid(which_normal = c("auto", "normal_na", "normal_bc"), historic_period = c(NA, "2001_2020"), 
+  argsCombos <- expand.grid(which_normal = c("auto", "normal_na"), historic_period = c(NA, "2001_2020"), 
                             historic_ts = c(NA, "1950:2010"), gcm_models = c(NA, "list_gcm()[1:3]"),
                             ssp = c(NA, "list_ssp()[1:3]"), gcm_period = c(NA, "list_gcm_period()[1:3]"),
-                            gcm_ts_years = c(NA, "2015:2050"), gcm_hist_years = c(NA, "1950:2010")) |>
+                            gcm_ts_years = c(NA, "2020:2050"), gcm_hist_years = c(NA, "1950:2010")) |>
     as.data.table()
+  
+  ## remove silly combos
+  argsCombos[is.na(gcm_models),
+             `:=`(gcm_period = NA, gcm_ts_years = NA, gcm_hist_years = NA, ssp = NA)]
+  argsCombos[is.na(gcm_period) & is.na(gcm_ts_years) & is.na(gcm_hist_years),
+             `:=`(gcm_models = NA, ssp = NA)]
+  argsCombos <- unique(argsCombos)
   
   out <- apply(argsCombos, 1, function(args, xyz) {
     args <- args[!is.na(args)]
@@ -133,13 +141,57 @@ test_that("test climr_dowscale with more args", {
     if (!is.null(args$gcm_ts_years)) {
       args$gcm_ts_years <- eval(parse(text = args$gcm_ts_years))
     }
-    if (!is.null(args$gcm_models)) {
+    if (!is.null(args$gcm_hist_years)) {
       args$gcm_hist_years <- eval(parse(text = args$gcm_hist_years))
     }
     
     out <- try(do.call(climr_downscale, args))
-    is(out, "data.frame")
+    
+    test <- is(out, "data.table")
+    test2 <- !any(c("ID", "Zone", "Subzone") %in% names(out))
+    test3 <- all(args$vars %in% names(out))
+    
+    ## check outputs of arguments that produce changing output columns
+    checkArgs <- setdiff(names(args), c("which_normal", "xyz", "vars"))
+    test4 <- logical(0)
+    i <- 1
+    if (length(checkArgs)) {
+      if ("historic_period" %in% checkArgs) {
+        test4[i] <- all(args$historic_period %in% unique(out$PERIOD))
+        i <- i + 1
+      } 
+      if ("historic_ts" %in% checkArgs) {
+        test4[i] <- all(args$historic_ts %in% unique(out$PERIOD))
+        i <- i + 1
+      } 
+      if ("gcm_models" %in% checkArgs) {
+        test4[i] <- all(args$gcm_models %in% unique(out$GCM))
+        i <- i + 1
+      } 
+      
+      if ("ssp" %in% checkArgs) {
+        test4[i] <- all(args$ssp %in% unique(out$SSP))
+        i <- i + 1
+      }
+      
+      if ("gcm_period" %in% checkArgs) {
+        test4[i] <- all(args$gcm_period %in% unique(out$PERIOD))
+        i <- i + 1
+      }
+      
+      if ("gcm_hist_years" %in% checkArgs) {
+        test4[i] <- all(args$gcm_hist_years %in% unique(out$PERIOD))
+        i <- i + 1
+      }
+      
+      if ("gcm_ts_years" %in% checkArgs) {
+        test4[i] <- all(args$gcm_ts_years %in% unique(out$PERIOD))
+        i <- i + 1
+      }
+    }
+    
+    return(list(test, test2, test3, test4))
   }, xyz = xyz)
   
-  browser()
+  expect_true(all(out))
 })
