e16db646250902fb596f49b846ab25835eb69a1d
CeresBarros <ceres.barros@gov.bc.ca>
2024-01-18 15:04:39 -0800

    added `.` to globalVars

--- R/globalVars.R
+++ R/globalVars.R
@@ -2,5 +2,5 @@ utils::globalVariables(c(
   "..cols", "..expectedCols", "fullnm", "id", "id_orig", "lat", "lon",
   "laynum", "mod", "numlay", "period", 
   "Period", "Period1", "Period2", "run", "Run",
-  "scenario", "Scenario", "skip_if_not_installed", "var", "Year"
+  "scenario", "Scenario", "skip_if_not_installed", "var", "Year", "."
 ))
