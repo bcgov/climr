fdbd85c7ba81d0ecdf77c8b04f352cb8da2a3ea2
CeresBarros <ceres.barros@gov.bc.ca>
2024-02-08 11:05:50 -0800

    more example data for vignettes

--- R/example_data.R
+++ R/example_data.R
@@ -15,6 +15,39 @@
 #' @source todo add/describe source
 "dem_vancouver"
 
+#' Example point locations
+#'
+#' Shapefile with point locations North Vancouver, BC, Canada.
+#' To use this data set, you need to `terra::unwrap`
+#' it first, e.g.:
+#' `vancouver_points <- terra::unwrap(get(data(vancouver_points)))`
+#'
+#' @format `PackedSpatVector`
+#' @source derived from `dem_vancouver`
+"vancouver_points"
+
+#' Example polygon locations
+#'
+#' Shapefile of North Vancouver in polygon format, BC, Canada.
+#' To use this data set, you need to `terra::unwrap`
+#' it first, e.g.:
+#' `vancouver_poly <- terra::unwrap(get(data(vancouver_poly)))`
+#'
+#' @format `PackedSpatVector`
+#' @source derived from `dem_vancouver`
+"vancouver_poly"
+
+#' Example grid of point locations for sampling
+#'
+#' Shapefile with point locations spaced at regular intervals, in
+#' North Vancouver, BC, Canada.
+#' To use this data set, you need to `terra::unwrap`
+#' it first, e.g.:
+#' `point_grid <- terra::unwrap(get(data(point_grid)))`
+#'
+#' @format `PackedSpatVector`
+#' @source derived from `dem_vancouver` reprojected at 0.01 degrees.
+"point_grid"
 
 #' Example Biogeoclimatic Ecological Classification Zones
 #'
