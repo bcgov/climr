99e128d5ab735fcb62c77607700531990e32e671
Mahony <Colin.Mahony@gov.bc.ca>
2024-05-09 16:38:12 -0700

    other variable name changes outside of core R scripts

--- man-roxygen/normal.R
+++ man-roxygen/normal.R
@@ -1,3 +1,3 @@
 #' @param normal A `character` or a `SpatRaster`. For character, label of the normal baseline to use.
 #'   Can be obtained from [`list_normal()`]. For `SpatRaster`, 36 layers normal climate variables with
-#'   names PPT01:PPT12, Tmax01:Tmax12 and Tmin01:Tmin12. Defaults to `list_normal()[1]`.
+#'   names PPT_01:PPT_12, Tmax_01:Tmax_12 and Tmin_01:Tmin_12. Defaults to `list_normal()[1]`.
