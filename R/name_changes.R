ddc8968145efad91b398313a59f642b99c5f2280
Mahony <Colin.Mahony@gov.bc.ca>
2024-05-23 06:56:09 -0700

    added name_changes to documentation

---
+++ R/name_changes.R
@@ -0,0 +1,13 @@
+#' Changes to naming conventions applied in Version 0.1.0
+#'
+#' A table of correspondence between old and new names for functions, parameters, and options. 
+#' For changes in climate variable names, see `data(variables)`.
+#'
+#' @format A `data.table` with columns:
+#' \describe{
+#'   \item{type}{The type of object that was renamed}
+#'   \item{function}{The function whose contents were renamed, if applicable}
+#'   \item{name_old}{The old name, pre-v0.1.0}
+#'   \item{name_new}{The new name, as of v0.1.0}
+#' }
+"name_changes"
