e31666b9a12301c842fd293ba1622435c3967802
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2024-07-26 22:23:21 -0700

    code readability (styler)

--- R/globalVars.R
+++ R/globalVars.R
@@ -1,11 +1,11 @@
 utils::globalVariables(c(
   "..cols", "..expectedCols", "C", "Code", "DATASET",
-  "degree", "element1", "element2", 
+  "degree", "element1", "element2",
   "fullnm", "GCM", "id", "id_orig", "lat", "lon",
   "laynum", "mod", "numlay", "period",
   "Period", "PERIOD", "Period1", "Period2", "run", "Run", "RUN",
   "scenario", "Scenario", "skip_if_not_installed", "SSP",
-  "xanom", "var", "variables", "Year", "yanom", 
-  "yeartime1", "yeartime2", 
+  "xanom", "var", "variables", "Year", "yanom",
+  "yeartime1", "yeartime2",
   ".", "%>%"
 ))
