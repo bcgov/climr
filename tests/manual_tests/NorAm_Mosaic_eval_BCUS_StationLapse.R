059668421fbdc964ed28b84c1f53e3b05ff7f14f
Colin Mahony <colin.mahony@gov.bc.ca>
2025-01-03 14:16:54 -0800

    edits to lapse rate analysis

--- tests/manual_tests/NorAm_Mosaic_eval_BCUS_StationLapse.R
+++ tests/manual_tests/NorAm_Mosaic_eval_BCUS_StationLapse.R
@@ -29,7 +29,7 @@ dem <- merge(dem.us, dem.bc)
 #---------------------------------
 
 e <- 1
-m <- 1
+m <- 3
 
 for(m in c(1,3,7,10)){
 
@@ -111,7 +111,7 @@ stn.tn.d <- stn.tn.o - stn.tn.r
 
 
 #============================================
-# moving window regression 
+# moving window regression
 #============================================
 
 # rasterize the station values (so that we can apply terra's focalReg function (for EDA and Plotting)
@@ -124,7 +124,7 @@ values(stn.el.o.pts) <- stn.el.o
 stn.el.o.raster <- rasterize(stn.el.o.pts, y=aggregate(dem, 3), field="value")
 # plot(stn.el.o.raster)
 
-# rasterize the station residuals so that we can apply terra's focalReg function 
+# rasterize the station residuals so that we can apply terra's focalReg function
 stn.tn.r.pts <- stn.pts
 values(stn.tn.r.pts) <- stn.tn.r
 stn.tn.r.raster <- rasterize(stn.tn.r.pts, y=aggregate(dem, 3), field="value")
@@ -133,67 +133,67 @@ stn.el.r.pts <- stn.pts
 values(stn.el.r.pts) <- stn.el.r
 stn.el.r.raster <- rasterize(stn.el.r.pts, y=aggregate(dem, 3), field="value")
 # plot(stn.el.r.raster)
-
-
+# 
+# 
+# 
+# #============================================
+# # scatterplots for selected areas (using residual values) BC - US Comparison
+# #============================================
+# 
+# plotarea <- ext(c(-119, -114, 47.5, 50.5))
+# 
+# par(mfrow=c(1,1))
+# png(filename=paste("tests/manual_tests/plots/prism_US_lapserates_residuals_", elements[e], monthcodes[m], ".png", sep=""), type="cairo", units="in", width=8, height=9, pointsize=14, res=600)
+# 
+# # window centroids
+# point.look <- matrix(c(-117.5, 49.8, -115.5, 49.8, -117.5, 48.35, -115.5, 48.35), nrow = 4, byrow = T)
+# box.size <- res(dem)*w
+# 
+# # mat <- cbind(c(1,2,4), c(1,3,5)); layout(mat)
+# mat <- rbind(c(1,1,1,1), c(1,1,1,1), c(1,1,1,1), c(2,3,4,5)); layout(mat)
+# par(mar=c(2,1,1,0.5), mgp=c(1.75, 0.25, 0))
+# 
+# X <- tn.r
+# lim <- max(values(tn.r), na.rm=T)
+# X[X>lim] <- lim
+# X[X < 0-lim] <- 0-lim
+# plot(crop(X, plotarea), legend=TRUE, type="continuous", col=rev(hcl.colors(99, "RdBu")), 
+#      main=paste(month.name[m], elements[e], "residuals ((\u00b0C) above regional average temperature)"), sep=" ")
+# points(stn.pts)
+# for(i in 1:dim(point.look)[1]){
+#   box.look <- ext(c(point.look[i,1]+box.size*c(-1, 1)/latFactor, point.look[i,2]+box.size*c(-1, 1)))
+#   plot(box.look, add=T)
+#   text(point.look[i,1]+box.size*0.8/latFactor, point.look[i,2]+box.size*0.8, i, cex=3, font=2)
+# }
+# 
+# par(mar=c(4,3,1,1), mgp=c(1.75, 0.25, 0))
+# for(i in 1:dim(point.look)[1]){
+#   box.look <- ext(c(point.look[i,1]+box.size*c(-1, 1)/latFactor, point.look[i,2]+box.size*c(-1, 1)))
+#   el.look <- values(crop(stn.el.r.raster, box.look))
+#   tn.look <- values(crop(stn.tn.r.raster, box.look))
+#   el.look <- el.look[!is.na(el.look)]
+#   tn.look <- tn.look[!is.na(tn.look)]
+#   dem.look <- values(crop(dem.r, box.look))
+#   prism.look <- values(crop(tn.r, box.look))
+#   
+#   plot(dem.look, prism.look, pch=16, cex=0.15, col="gray", xlab="Residual elevation (m)", tck = -0.01, ylab = "Residual temperature (\u00b0C)")
+#   points(el.look, tn.look, pch=16, cex=1.5, col="blue")
+#   lm <- lm(tn.look~el.look)
+#   abline(lm)
+#   mtext(i, adj=0.95, line = -2.5, side=3, cex=1.5, font=2)
+#   mtext(paste("lapse =", round(lm$coefficients[2]*1000, 1), "(\u00b0C)/km"), adj=0.05, line = -1.5, side=1, cex = 0.6)
+# }
+# # no evidence for inversion effects even in january
+# dev.off()
 
 #============================================
-# scatterplots for selected areas (using residual values) BC - US Comparison
+# scatterplots for selected areas (using raw values) BC - US Comparison
 #============================================
 
 plotarea <- ext(c(-119, -114, 47.5, 50.5))
 
 par(mfrow=c(1,1))
-png(filename=paste("results/prism_US_lapserates_residuals_", elements[e], monthcodes[m], ".png", sep=""), type="cairo", units="in", width=8, height=9, pointsize=14, res=600)
-
-# window centroids
-point.look <- matrix(c(-117.5, 49.8, -115.5, 49.8, -117.5, 48.35, -115.5, 48.35), nrow = 4, byrow = T)
-box.size <- res(dem)*w
-
-# mat <- cbind(c(1,2,4), c(1,3,5)); layout(mat)
-mat <- rbind(c(1,1,1,1), c(1,1,1,1), c(1,1,1,1), c(2,3,4,5)); layout(mat)
-par(mar=c(2,1,1,0.5), mgp=c(1.75, 0.25, 0))
-
-X <- tn.r
-lim <- max(values(tn.r), na.rm=T)
-X[X>lim] <- lim
-X[X < 0-lim] <- 0-lim
-plot(crop(X, plotarea), legend=TRUE, type="continuous", col=rev(hcl.colors(99, "RdBu")), 
-     main=paste(month.name[m], elements[e], "residuals (Kelvins above regional average temperature)"), sep=" ")
-points(stn.pts)
-for(i in 1:dim(point.look)[1]){
-  box.look <- ext(c(point.look[i,1]+box.size*c(-1, 1)/latFactor, point.look[i,2]+box.size*c(-1, 1)))
-  plot(box.look, add=T)
-  text(point.look[i,1]+box.size*0.8/latFactor, point.look[i,2]+box.size*0.8, i, cex=3, font=2)
-}
-
-par(mar=c(4,3,1,1), mgp=c(1.75, 0.25, 0))
-for(i in 1:dim(point.look)[1]){
-  box.look <- ext(c(point.look[i,1]+box.size*c(-1, 1)/latFactor, point.look[i,2]+box.size*c(-1, 1)))
-  el.look <- values(crop(stn.el.r.raster, box.look))
-  tn.look <- values(crop(stn.tn.r.raster, box.look))
-  el.look <- el.look[!is.na(el.look)]
-  tn.look <- tn.look[!is.na(tn.look)]
-  dem.look <- values(crop(dem.r, box.look))
-  prism.look <- values(crop(tn.r, box.look))
-  
-  plot(dem.look, prism.look, pch=16, cex=0.15, col="gray", xlab="Residual elevation (m)", tck = -0.01, ylab = "Residual temperature (K)")
-  points(el.look, tn.look, pch=16, cex=1.5, col="blue")
-  lm <- lm(tn.look~el.look)
-  abline(lm)
-  mtext(i, adj=0.95, line = -2.5, side=3, cex=1.5, font=2)
-  mtext(paste("lapse =", round(lm$coefficients[2]*1000, 1), "K/km"), adj=0.05, line = -1.5, side=1, cex = 0.6)
-}
-# no evidence for inversion effects even in january
-dev.off()
-
-#============================================
-# scatterplots for selected areas (using residual values) BC - US Comparison
-#============================================
-
-plotarea <- ext(c(-119, -114, 47.5, 50.5))
-
-par(mfrow=c(1,1))
-png(filename=paste("results/prism_US_lapserates_raw_", elements[e], monthcodes[m], ".png", sep=""), type="cairo", units="in", width=8, height=9, pointsize=14, res=600)
+png(filename=paste("tests/manual_tests/plots/prism_US_lapserates_raw_", elements[e], monthcodes[m], ".png", sep=""), type="cairo", units="in", width=8, height=9, pointsize=14, res=600)
 
 # window centroids
 point.look <- matrix(c(-117.5, 49.8, -115.5, 49.8, -117.5, 48.35, -115.5, 48.35), nrow = 4, byrow = T)
@@ -204,8 +204,8 @@ mat <- rbind(c(1,1,1,1), c(1,1,1,1), c(1,1,1,1), c(2,3,4,5)); layout(mat)
 par(mar=c(2,1,1,0.5), mgp=c(1.75, 0.25, 0))
 
 plot(crop(tn.o, plotarea), legend=TRUE, type="continuous", col=rev(hcl.colors(99, "RdBu")), 
-     main=paste(month.name[m], elements[e], "(degrees Celsius)", sep=" "))
-points(stn.pts)
+     main=paste(month.name[m], elements[e], "(\u00b0C)", sep=" "))
+plot(stn.pts, add=T)
 for(i in 1:dim(point.look)[1]){
   box.look <- ext(c(point.look[i,1]+box.size*c(-1, 1)/latFactor, point.look[i,2]+box.size*c(-1, 1)))
   plot(box.look, add=T)
@@ -222,14 +222,60 @@ for(i in 1:dim(point.look)[1]){
   dem.look <- values(crop(dem.o, box.look))
   prism.look <- values(crop(tn.o, box.look))
   
-  plot(dem.look, prism.look, pch=16, cex=0.15, col="gray", xlab="Elevation (m)", tck = -0.01, ylab = "Temperature (K)")
-  points(el.look, tn.look, pch=16, cex=1.5, col="blue")
+  plot(dem.look, prism.look, pch=16, cex=0.15, col="gray", xlab="Elevation (m)", tck = -0.01, ylab = "Temperature (\u00b0C)")
+  points(el.look, tn.look, pch=16, cex=1)
   lm <- lm(tn.look~el.look)
   abline(lm)
   mtext(i, adj=0.95, line = -2.5, side=3, cex=1.5, font=2)
-  mtext(paste("lapse =", round(lm$coefficients[2]*1000, 1), "K/km"), adj=0.05, line = -1.5, side=1, cex = 0.6)
+  mtext(paste("lapse =", round(lm$coefficients[2]*1000, 1), "(\u00b0C)/km"), adj=0.05, line = -1.5, side=1, cex = 0.6)
 }
-# no evidence for inversion effects even in january
+# not much evidence for inversion effects even in january
+dev.off()
+
+#============================================
+# scatterplots for selected areas (using raw values) BC - US Comparison
+#============================================
+
+plotarea <- ext(c(-116.5, -114, 47.5, 49))
+
+par(mfrow=c(1,1))
+png(filename=paste("tests/manual_tests/plots/prism_US_lapserates_raw_Montana_", elements[e], monthcodes[m], ".png", sep=""), type="cairo", units="in", width=8, height=9, pointsize=14, res=600)
+
+# window centroids
+point.look <- matrix(c(-115.875, 48.625, -114.625, 48.625, -115.875, 47.875, -114.625, 47.875), nrow = 4, byrow = T)
+box.size <- res(dem)*w/2
+
+# mat <- cbind(c(1,2,4), c(1,3,5)); layout(mat)
+mat <- rbind(c(1,1,1,1), c(1,1,1,1), c(1,1,1,1), c(2,3,4,5)); layout(mat)
+par(mar=c(2,1,1,0.5), mgp=c(1.75, 0.25, 0))
+
+plot(crop(tn.o, plotarea), legend=TRUE, type="continuous", col=rev(hcl.colors(99, "RdBu")), 
+     main=paste(month.name[m], elements[e], "(\u00b0C)", sep=" "))
+plot(stn.pts, add=T)
+for(i in 1:dim(point.look)[1]){
+  box.look <- ext(c(point.look[i,1]+box.size*c(-1, 1)/latFactor, point.look[i,2]+box.size*c(-1, 1)))
+  plot(box.look, add=T)
+  text(point.look[i,1]+box.size*0.8/latFactor, point.look[i,2]+box.size*0.8, i, cex=3, font=2)
+}
+par(mar=c(4,3,1,1), mgp=c(1.75, 0.25, 0))
+for(i in 1:dim(point.look)[1]){
+  box.look <- ext(c(point.look[i,1]+box.size*c(-1, 1)/latFactor, point.look[i,2]+box.size*c(-1, 1)))
+  el.look <- values(crop(stn.el.o.raster, box.look))
+  tn.look <- values(crop(stn.tn.o.raster, box.look))
+  el.look <- el.look[!is.na(el.look)]
+  tn.look <- tn.look[!is.na(tn.look)]
+  dem.look <- values(crop(dem.o, box.look))
+  prism.look <- values(crop(tn.o, box.look))
+  
+  plot(dem.look, prism.look, pch=16, cex=0.15, col="gray", xlab="Elevation (m)", tck = -0.01, ylab = "Temperature (\u00b0C)")
+  points(el.look, tn.look, pch=16, cex=1)
+  lm <- lm(tn.look~el.look)
+  abline(lm)
+  mtext(i, adj=0.95, line = -2.5, side=3, cex=1.5, font=2)
+  mtext(paste("lapse =", round(lm$coefficients[2]*1000, 1), "(\u00b0C)/km"), adj=0.05, line = -1.5, side=1, cex = 0.6)
+}
+# no evidence for inverse-tilda effect
+
 dev.off()
 
 print(m)
