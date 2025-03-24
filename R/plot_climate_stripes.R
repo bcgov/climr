37587e0e40ed35ac7ac0b76c158145157624814d
Bruno Tremblay <bruno@boostao.ca>
2025-03-24 16:09:55 -0400

    merging stripes

--- R/plot_climate_stripes.R
+++ R/plot_climate_stripes.R
@@ -4,8 +4,10 @@
 #' Plot temperature change relative to average over time 
 #'
 #'
-#' @param X  A `data.table` object that contains the change in temperature relative to the average and the corresping years
-#' @param mode  character string in warming_stripes, labelled_stripes, bars
+#' @param x A `data.table` object that contains the change in temperature relative to the average
+#' and the corresponding years.
+#' @param mode A character string. Either `warming_stripes`, `labelled_stripes`, `bars` or 
+#' `bars_with_scale`.
 #' 
 #'
 #' @return plotly object, Draws a plot in the active graphics device.
@@ -19,9 +21,9 @@
 #' @importFrom scales rescale
 #'
 #' @export
-
-
-plot_climateStripes <- function() {
+plot_climate_stripes <- function(X, mode = c("warming_stripes", "labelled_stripes", "bars", "bars_with_scale")) {
+  
+  mode <- match.arg(mode)
   
   # options
   negative_climate_color_ramp <- colorRamp(colors = c("#0a306b","#08519c","#2270b5","#4192c5","#95bcd0","#c6daef","#d5e4f4"))
@@ -29,28 +31,30 @@ plot_climateStripes <- function() {
   
   #TODO handle input data
   # mock data
-  X <- data.table(x = seq(1850, 2023, by = 2))
-  X[, y := 0.00867052*x - 16.99 + runif(.N, -0.1, 0.1)]
-  X[y <= 0, marker_color := convert_rgb_to_col(negative_climate_color_ramp(rescale(y, to = c(0,1), from = c(min(y), 0))))]
-  X[y >0, marker_color := convert_rgb_to_col(positive_climate_color_ramp(rescale(y, to = c(0,1), from = c(0, max(y)))))]
+  if (missing(x)) {
+    X <- data.table(x = seq(1850, 2023, by = 2))
+    X[, y := 0.00867052*x - 16.99 + runif(.N, -0.1, 0.1)]
+    X[y <= 0, marker_color := convert_rgb_to_col(negative_climate_color_ramp(rescale(y, to = c(0,1), from = c(min(y), 0))))]
+    X[y >0, marker_color := convert_rgb_to_col(positive_climate_color_ramp(rescale(y, to = c(0,1), from = c(0, max(y)))))]
+  }
   # TODO plot 
-  if (mode == "warming_stripes") {
+  if (mode %in% "warming_stripes") {
     plot_ly(X) |>
       add_bars(x = ~x, y = ~1, marker = list(color = ~marker_color)) |>
       layout(bargap = 0, xaxis = list(visible = FALSE), yaxis = list(visible = FALSE))
   }
-  if (mode == "labelled_stripes") {
+  if (mode %in% "labelled_stripes") {
     plot_ly(X) |>
       add_bars(x = ~x, y = ~1, marker = list(color = ~marker_color)) |>
       layout(title = list(text = "Global temperature change (1850-2023)"),bargap = 0, xaxis = list(showgrid = FALSE, showline = FALSE, title = FALSE), yaxis = list(visible = FALSE))
   }
-  if (mode == "bars") {
+  if (mode %in% "bars") {
     plot_ly(X) |>
       add_bars(x = ~x, y = ~y, marker = list(color = ~marker_color)) |>
       layout(title = list(text = paste0("Global temperature have increase by over ", X[, round(max(y)- min(y),1)], "°C")),bargap = 0, xaxis = list(visible = FALSE), yaxis = list(visible = FALSE))
     
   }
-  if (mode == "bars_with_scale") {
+  if (mode %in% "bars_with_scale") {
     plot_ly(X) |>
       add_bars(x = ~x, y = ~y, marker = list(color = ~marker_color)) |>
       layout(bargap = 0, xaxis = list(showgrid = FALSE,showline = TRUE, title = FALSE), yaxis = list(showgrid = FALSE, showline = TRUE, title = FALSE))
@@ -58,7 +62,7 @@ plot_climateStripes <- function() {
 
  }
 
-
+#' @noRd
 convert_rgb_to_col <- function(rgb_matrix) {
   rgb(rgb_matrix[,1], rgb_matrix[,2], rgb_matrix[,3], maxColorValue = 255) 
 }
