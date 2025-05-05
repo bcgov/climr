7e4f03145e2fe2444603f6497817c66bfe72dd34
Colin Mahony <colin.mahony@gov.bc.ca>
2025-05-05 15:13:06 -0700

    updates for testing as a fixed issue

--- tests/manual_tests/EDA_CNRM_ts.R
+++ tests/manual_tests/EDA_CNRM_ts.R
@@ -7,18 +7,19 @@ library(dplyr)
 # Get lat, long, and elevation for 7 locations in BC
 test_pts<-data.frame(id = seq(1,6,by=1),
                      lon = c(-120.1879,-120.4258,-121.9251,-120.3030,-127.5062,-127.6785),
-                     lat = c(59.3396, 57.4644, 59.9900, 55.2420, 54.0191, 54.1638),
+                     lat = c(55, 57.4644, 59.9900, 55.2420, 54.0191, 54.1638),
                      elev = c(441.9092,901.2709,461.7851,926.7590,1098.2932,1022.2858)
 )
 
 ds_out <- downscale(
   xyz = test_pts,
-  which_refmap = "auto",
+  which_refmap = "refmap_climr",
   gcm_hist_years = 1960:2014,
   gcm_ssp_years = 2015:2100,
   ssps = c("ssp245"),
   max_run = 3,
   gcms = list_gcms()[3:4],
+  db_option = "local",
   vars = c("Tmax_sm", "PPT_sm"))
 
 ds_out2<-ds_out[!is.na(GCM),]
@@ -44,7 +45,6 @@ for(i in unique(ds_out2[GCM=="CNRM-ESM2-1", RUN])){
 
 ds_out <- downscale(
   xyz = test_pts[1,],
-  which_refmap = "auto",
   gcm_hist_years = 1960:2014,
   gcm_ssp_years = 2015:2100,
   ssps = c("ssp245"),
@@ -81,16 +81,17 @@ for(ssp in list_ssps()[-4]){
 
 ds_out <- downscale(
   xyz = test_pts[1,],
-  which_refmap = "auto",
   gcm_hist_years = 1960:2014,
   gcm_ssp_years = 2015:2100,
   ssps = list_ssps()[2],
   max_run = 1,
   gcms = list_gcms()[4],
+  db_option = "local",
   vars = list_vars(set="Seasonal"))
 
 par(mfrow=c(4,3), mar=c(2,2,2,0.1), mgp=c(3, 0.25, 0), tck=-0.01)
 vars = as.vector(outer(c("Tmin", "Tmax", "PPT"), c("wt", "sp", "sm", "at"), paste, sep = "_"))
+ssp = list_ssps()[2]
 run <- unique(ds_out[RUN != "ensembleMean" & SSP==ssp, RUN])
 for(var in vars){
     x <- ds_out[RUN != "ensembleMean" & RUN==run, PERIOD]
