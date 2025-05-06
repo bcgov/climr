585f2f110f29bdc108b4322c88331985be69897d
Colin Mahony <colin.mahony@gov.bc.ca>
2025-05-06 16:59:37 -0700

    investigate speckling

--- tests/manual_tests/check_CompareClimateNA.R
+++ tests/manual_tests/check_CompareClimateNA.R
@@ -12,7 +12,7 @@ my_grid <- as.data.frame(dem, cells = TRUE, xy = TRUE)
 colnames(my_grid) <- c("id", "lon", "lat", "elev") # rename column names to what climr expects
 
 ## climr data
-climr <- downscale(xyz = my_grid, which_refmap = "refmap_climatena", vars = list_vars())
+climr <- downscale(xyz = my_grid, vars = list_vars())
 
 ## climateNA data 
 climna_grid <- my_grid[,c(1,1,2,3,4)]
@@ -30,6 +30,7 @@ vars <- list_vars()
 vars <- vars[-which(vars%in%c("Tave", "Tmin", "Tmax"))]
 
 # for(var in vars[grep("NFFD", vars)]){
+var="PPT_07"
 for(var in vars){
   png(filename=paste(figDir, "/climrVclimna", var, "png",sep="."), type="cairo", units="in", width=6.5, height=2, pointsize=10, res=600)
   
@@ -39,6 +40,9 @@ for(var in vars){
   data_climr <-   climr[,get(var)]
   X[climr[, id]] <- data_climr
   plot(X, main=paste("climr", var), axes=F)
+  
+  s <- which(data_climr>1000)
+  points(my_grid$lon[s], my_grid$lat[s])
   
   var_climna <- variables$Code_ClimateNA[which(variables$Code==var)]
   data_climna <- climna[,get(var_climna)]
@@ -64,4 +68,76 @@ for(var in vars){
   print(var)
   dev.off()
 }
+
+
+#----------------------------
+# Investigate speckling
+
+# compare refmap to downscaled
+
+## get bounding box based on input points
+thebb <- get_bb(my_grid)
+
+## get database connection
+dbCon <- data_connect()
+
+# obtain the climatena 1961-1990 normals for the study area. 
+refmap <- input_refmap(dbCon, thebb, reference = "refmap_climr")
+
+var="Tmin_01"
+par(mfrow=c(1,2), mar=c(0,0,2,2))
+values(X) <- NA
+
+plot(refmap[[names(refmap)==var]], main=paste("climr refmap", var), axes=F)
+
+data_climr <-   climr[,get(var)]
+X[climr[, id]] <- data_climr
+plot(X, main=paste("climr downscaled", var), axes=F)
+s <- which(data_climr>100)
+points(my_grid$lon[s], my_grid$lat[s])
+
+
+# Zoom in and investigate lapse rates
+
+var="Tmin_01"
+par(mfrow=c(1,3), mar=c(0,0,2,2))
+values(X) <- NA
+
+studyarea <- ext(c(-121, -118, 65, 66.5))
+
+plot(refmap[[names(refmap)==var]], main=paste("climr refmap", var), xlim=studyarea[1:2], ylim=studyarea[3:4])
+
+data_climr <-   climr[,get(var)]
+X[climr[, id]] <- data_climr
+plot(X, main=paste("climr downscaled", var), xlim=studyarea[1:2], ylim=studyarea[3:4])
+s <- which(data_climr>100)
+points(my_grid$lon[s], my_grid$lat[s])
+
+plot(refmap[[grep(var, names(refmap))[2]]], main=paste("climr lapse rate", var), xlim=studyarea[1:2], ylim=studyarea[3:4])
+s <- which(data_climr > 100)
+points(my_grid$lon[s], my_grid$lat[s])
+
+# aggregate lapse rates to downscaled resolution
+
+var="Tmin_01"
+par(mfrow=c(1,3), mar=c(0,0,2,2))
+values(X) <- NA
+
+studyarea <- ext(c(-121, -118, 65, 66.5))
+
+plot(refmap[[names(refmap)==var]], main=paste("climr refmap", var), xlim=studyarea[1:2], ylim=studyarea[3:4])
+
+data_climr <-   climr[,get(var)]
+X[climr[, id]] <- data_climr
+plot(X, main=paste("climr downscaled", var), xlim=studyarea[1:2], ylim=studyarea[3:4])
+s <- which(data_climr>100)
+points(my_grid$lon[s], my_grid$lat[s])
+
+lr <- crop(refmap[[grep(var, names(refmap))[2]]], studyarea)
+lr <- project(lr, dem)
+plot(lr, main=paste("climr lapse rate", var), xlim=studyarea[1:2], ylim=studyarea[3:4])
+s <- which(data_climr > 100)
+points(my_grid$lon[s], my_grid$lat[s])
+
+
 
