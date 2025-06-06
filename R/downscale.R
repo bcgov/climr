bef850b96c334faf258cd45358b3a3d4db121330
Kiri <kiridaust@gmail.com>
2025-06-06 12:06:27 -0700

    another bug!

--- R/downscale.R
+++ R/downscale.R
@@ -274,7 +274,9 @@ downscale <- function(xyz, which_refmap = "refmap_climr",
   results <- results_ts <- NULL
   return_ref_orig <- return_refperiod
   if(any(!is.null(c(obs_periods_db, obs_years_db, gcm_ssp_periods_db, gcm_ssp_ts, gcm_hist_ts)))) return_refperiod <- FALSE
-  if(any(!is.null(c(obs_periods_reg, obs_years_reg, gcm_ssp_periods, gcm_ssp_ts_reg, gcm_hist_ts_reg))) | (return_ref_orig & db_option == "local")){
+  if(any(!is.null(c(obs_periods_reg, obs_years_reg, gcm_ssp_periods, gcm_ssp_ts_reg, gcm_hist_ts_reg))) | 
+     (return_ref_orig & db_option == "local") | 
+     inherits(xyz, "SpatRaster")){
     reference <- input_refmap(reference = which_refmap, bbox = thebb, cache = cache, indiv_tiles = indiv_tiles, xyz = xyz)
     message("Downscaling...")
     results <- downscale_core(
@@ -291,7 +293,7 @@ downscale <- function(xyz, which_refmap = "refmap_climr",
     )
   }
   
-  if(any(!is.null(c(obs_periods_db, obs_years_db, gcm_ssp_periods_db, gcm_ssp_ts, gcm_hist_ts))) | return_ref_orig){ ##always returns refperiod
+  if(any(!is.null(c(obs_periods_db, obs_years_db, gcm_ssp_periods_db, gcm_ssp_ts, gcm_hist_ts))) | (return_ref_orig & !inherits(xyz, "SpatRaster"))){ ##always returns refperiod
     write_xyz(xyz)
     reference_db <- input_refmap_db(reference = which_refmap)
     message("Downscaling in database...")
